f317fda76905588d29a30882a283fcdd07fcb439
FedG-code <federico.giovannini28@gmail.com>
2024-09-12 18:49:16 +0100

    directional cloud logic, warning panel script generation

--- scripts/death_cloud.gd
+++ scripts/death_cloud.gd
@@ -6,7 +6,9 @@ signal cloud_hit_player
 var move_direction = Vector2.ZERO
 var is_moving = false
 var screen_width = 0
+var screen_height = 0
 var cloud_width = 0
+var cloud_height = 0
 var camera = Node2D
 
 func _ready():
@@ -18,9 +20,11 @@ func _ready():
 	
 	var viewport_size = get_viewport_rect().size
 	screen_width = viewport_size.x / camera.zoom.x
+	screen_height = viewport_size.y / camera.zoom.y
 	
 	var used_rect = $CloudTilemap.get_used_rect()
 	cloud_width = used_rect.size.x * $CloudTilemap.tile_set.tile_size.x
+	cloud_height = used_rect.size.y * $CloudTilemap.tile_set.tile_size.y
 	$CloudArea.body_entered.connect(Callable(self, "_on_cloud_body_entered"))
 
 func _process(delta):
@@ -29,25 +33,30 @@ func _process(delta):
 		if cloud_covers_screen():
 			is_moving = false
 
-func start_cloud_movement():
+func start_cloud_movement(direction):
 	visible = true
 	is_moving = true
+	move_direction = direction
 	var start_from_left = randf() > 0.5
 	if start_from_left:
 		position.x = camera.get_screen_center_position().x - (screen_width / 2) - cloud_width
-		move_direction = Vector2.RIGHT
 	else:
 		position.x = camera.get_screen_center_position().x + (screen_width / 2)
-		move_direction = Vector2.LEFT
 	
 	position.y = -10
 
 func cloud_covers_screen() -> bool:
 	var screen_center_x = camera.get_screen_center_position().x
+	var screen_center_y = camera.get_screen_center_position().y
+
 	if move_direction == Vector2.RIGHT:
 		return position.x + cloud_width >= screen_center_x + (screen_width / 2) +10
-	else:
+	if move_direction == Vector2.LEFT:
 		return position.x <= screen_center_x - (screen_width / 2) -10
+	if move_direction == Vector2.UP:
+		return position.y + cloud_height >= screen_center_y + (screen_height / 2) + 10
+	else:
+		return position.y <= screen_center_y - (screen_height / 2) - 10
 
 func _on_cloud_body_entered(body):
 	if body.is_in_group("player"):
